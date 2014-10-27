e7a67252195107799d42b03c1529bfa6692197c5
Your Name <you@example.com>
2014-10-27 16:12:33 +0000

    graph out

---
+++ licmgmt/matplot.py
@@ -0,0 +1,8 @@
+__author__ = 'pgrant'
+
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4])
+plt.plot([3,5,7,9])
+plt.axis([0, 10, 0, 20])
+plt.ylabel('some numbers')
+plt.show()
--- licmgmt/maxlic2.py
+++ licmgmt/maxlic2.py
@@ -1,7 +1,8 @@
 __author__ = 'pgrant'
 
-__author__ = 'pgrant'
 from copy import deepcopy
+import matplotlib.pyplot as plt
+import numpy as np
 
 dateSet = 0
 periodCountTable = {}
@@ -10,24 +11,43 @@ maxCountTable = {}
 maxDeniedCountTable = {}
 lastMaxCountTable = {}
 lastDate = ""
+plotDict = {}
+plotDict['xAxis'] = {}
+plotList = {}
+
+mapIndex = 0
 
 maxLic = 0
-for line in open('/home/pgrant/workarea/python/arm-20140715-095116.log'):
+#for line in open('/home/pgrant/workarea/python/arm-20140715-095116.log'):
+for line in open('/home/pgrant/workarea/python/test.log'):
     tokenList = line.split(" ")
     if "TIMESTAMP" in tokenList:
         dateSet = 1
+        tokenList[-1] = tokenList[-1][:-1]
         if tokenList[0] == '':
             del tokenList[0]
         if lastDate  != tokenList[-1]:
             lastDate  = tokenList[-1]
-            print("\n")
-            print(tokenList[-1], " , ", tokenList[0], ", ")
+            xAxis = plotDict['xAxis']
+            xAxis[mapIndex] = tokenList[-1]
+ #           print(tokenList[-1], " , ")
             for licName in maxCountTable.keys():
-                print(licName, ", ", maxCountTable[licName], ", ")
-            print("\n")
-            print(tokenList[-1], " , ", tokenList[0], ", ")
+                if licName not in plotDict:
+                    plotDict[licName] = {}
+            for licName in maxCountTable.keys():
+#                print(licName, ", ", maxCountTable[licName], ", ")
+                plotDict[licName][mapIndex] = maxCountTable[licName]
+#            print("\n")
+#            print(tokenList[-1], ", ")
             for licName in maxDeniedCountTable.keys():
-                print(licName, ", ", maxDeniedCountTable[licName], ", ")
+                deniedLicName = 'denied' + ':' + licName
+                if deniedLicName not in plotDict:
+                    plotDict[deniedLicName] = {}
+            for licName in maxDeniedCountTable.keys():
+                deniedLicName = 'denied' + ':' + licName
+#               print(deniedLicName, ", ", maxDeniedCountTable[licName], ", ")
+                plotDict[deniedLicName][mapIndex] = maxDeniedCountTable[licName]
+            mapIndex = mapIndex + 1
     if dateSet == 1:
         if "OUT:" in tokenList:
             if  "OUT:" in tokenList[3]:
@@ -35,42 +55,77 @@ for line in open('/home/pgrant/workarea/python/arm-20140715-095116.log'):
             else:
                 tokenPos = 3
 
-            if tokenList[tokenPos] in periodDeniedCountTable:
-                if tokenList[tokenPos] in maxDeniedCountTable:
-                    if periodDeniedCountTable[tokenList[tokenPos]] > maxDeniedCountTable[tokenList[tokenPos]]:
-                        maxDeniedCountTable[tokenList[tokenPos]] =  periodDeniedCountTable[tokenList[tokenPos]]
+            licName = tokenList[tokenPos]
+            licName = licName[:-1]
+            licName = licName[1:]
+
+            if licName in periodDeniedCountTable:
+                if licName in maxDeniedCountTable:
+                    if periodDeniedCountTable[licName] > maxDeniedCountTable[licName]:
+                        maxDeniedCountTable[licName] =  periodDeniedCountTable[licName]
                 else:
-                    maxDeniedCountTable[tokenList[tokenPos]] =  periodDeniedCountTable[tokenList[tokenPos]]
-                periodDeniedCountTable[tokenList[tokenPos]] = 0
-            if tokenList[tokenPos] in periodCountTable:
-                periodCountTable[tokenList[tokenPos]] =  periodCountTable[tokenList[tokenPos]] + 1
-                if periodCountTable[tokenList[tokenPos]] > maxCountTable[tokenList[tokenPos]]:
-                    maxCountTable[tokenList[tokenPos]] = periodCountTable[tokenList[tokenPos]]
+                    maxDeniedCountTable[licName] =  periodDeniedCountTable[licName]
+                periodDeniedCountTable[licName] = 0
+            if licName in periodCountTable:
+                periodCountTable[licName] =  periodCountTable[licName] + 1
+                if periodCountTable[licName] > maxCountTable[licName]:
+                    maxCountTable[licName] = periodCountTable[licName]
             else:
-                periodCountTable[tokenList[tokenPos]] = 1
-                if tokenList[tokenPos] not in maxCountTable:
-                    maxCountTable[tokenList[tokenPos]] = 1
+                periodCountTable[licName] = 1
+                if licName not in maxCountTable:
+                    maxCountTable[licName] = 1
 
         if "IN:" in tokenList:
             if  "IN:" in tokenList[3]:
                 tokenPos = 4
             else:
                 tokenPos = 3
-            if tokenList[tokenPos] in periodCountTable:
-                if periodCountTable[tokenList[tokenPos]] > 0:
-                    periodCountTable[tokenList[tokenPos]] = periodCountTable[tokenList[tokenPos]] - 1
+
+            licName = tokenList[tokenPos]
+            licName = licName[:-1]
+            licName = licName[1:]
+
+            if licName in periodCountTable:
+                if periodCountTable[licName] > 0:
+                    periodCountTable[licName] = periodCountTable[licName] - 1
                 else:
-                    periodCountTable[tokenList[tokenPos]] = 0
+                    periodCountTable[licName] = 0
 
         if "DENIED:" in tokenList:
             if  "IN:" in tokenList[3]:
                 tokenPos = 4
             else:
                 tokenPos = 3
-            if tokenList[tokenPos] in periodDeniedCountTable:
-                periodDeniedCountTable[tokenList[tokenPos]] = periodDeniedCountTable[tokenList[tokenPos]] + 1
+            if licName in periodDeniedCountTable:
+                periodDeniedCountTable[licName] = periodDeniedCountTable[licName] + 1
             else:
-                periodDeniedCountTable[tokenList[tokenPos]] = 1
+                periodDeniedCountTable[licName] = 1
 
+for licName in plotDict:
+    plotList[licName] = []
+for indexCount in range(0, mapIndex):
+    for licName in plotDict:
+        if indexCount in plotDict[licName]:
+            plotList[licName].append(plotDict[licName][indexCount])
+        else:
+            plotList[licName].append(0)
+fig = plt.figure()
+ax = plt.subplots(111)
+
+for licName in plotList:
+    if licName !=  'xAxis':
+        ax.plt(plotList[licName], label=licName)
+
+legend = ax.legend(loc='upper center', shadow=True)
+frame = legend.get_frame()
+frame.set_facecolor('0.90')
+
+# Set the fontsize
+for label in legend.get_texts():
+    label.set_fontsize('large')
+
+for label in legend.get_lines():
+    label.set_linewidth(1.5)  # the legend line widt
+plt.show()
 
 
